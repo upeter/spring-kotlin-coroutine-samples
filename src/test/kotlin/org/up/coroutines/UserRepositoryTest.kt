d065d2a3a1d304ae445af43869492220000fb964
upeter <peter.urs@gmail.com>
2020-04-29 20:11:54 +0200

    Coroutine db test

---
+++ src/test/kotlin/org/up/coroutines/UserRepositoryTest.kt
@@ -0,0 +1,29 @@
+package org.up.coroutines
+
+import io.kotlintest.shouldBe
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.transaction.annotation.Transactional
+import org.up.coroutines.model.User
+import org.up.coroutines.repository.UserRepository
+
+
+@SpringBootTest
+@Transactional
+@ExtendWith(SpringExtension::class)
+class UserRepositoryTest @Autowired constructor(
+        val userRepository: UserRepository
+) {
+
+    @Test
+    fun `should insert new user and find it by Id`() = runBlocking{
+        val newUser = userRepository.save(User(userName = "Joe", email = "joe@home.nl"))
+        val foundUser = userRepository.findById(newUser.id!!)
+        foundUser shouldBe newUser
+
+    }
+}
